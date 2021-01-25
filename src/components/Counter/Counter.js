680e16a9550f160607e08ea37993effcdc999da1
Verica Zoksimovska <vericaz1809@gmail.com>
2021-01-25 19:54:50 +0100

    counter

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,15 @@
+// const ConnectedComponent = connect(Counter);
+//export default ConnectedComponent
+
+import React from "react";
+import { connect } from "react-redux";
+
+const Counter = (props) => {
+  return <p style={{ fontSize: "40px", margin: "20px" }}>{props.counter}</p>;
+};
+
+const mapStateToProps = (state) => ({
+  counter: state.counter,
+});
+
+export default connect(mapStateToProps)(Counter);
